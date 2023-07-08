1115ff2de5f384eb7a9b3d860212040aab0aad96
____marcell <0000marcell@gmail.com>
2023-07-08 15:42:42 +0000

    package

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='python_remote_module',
+    version='1.0.0',
+    author='Marcell Cruz',
+    author_email='',
+    description='Description of your package',
+    packages=find_packages(),
+    install_requires=[],
+)
